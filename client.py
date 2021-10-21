9620c0be9e79405c86eb1247711b3d613b714f2c
eeappiah <66851526+eeappiah@users.noreply.github.com>
2021-10-21 16:54:49 +0100

    update on client and server files done

--- client.py
+++ client.py
@@ -123,7 +123,9 @@ def main():
 
             win.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
             pygame.display.update()
+            print("hi")
             pygame.time.delay(2000)
+            print("hi")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -142,62 +144,6 @@ def main():
                                 n.send(btn.text)
 
         redrawWindow(win, game, player)
-
-
-run = True
-clock = pygame.time.Clock()
-n = Network()
-player = int(n.getP())
-print("You are player", player)
-
-while run:
-    clock.tick(60)
-    try:
-        game = n.send("get")
-    except:
-        run = False
-        print("Couldn't get game")
-        break
-
-    if game.bothWent():
-        redrawWindow(win, game, player)
-        pygame.time.delay(500)
-        try:
-            game = n.send("reset")
-        except:
-            run = False
-            print("Couldn't get game")
-            break
-
-        font = pygame.font.SysFont("comicsans", 90)
-        if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
-            text = font.render("You Won!", 1, (255, 0, 0))
-        elif game.winner() == -1:
-            text = font.render("Tie Game!", 1, (255, 0, 0))
-        else:
-            text = font.render("You Lost...", 1, (255, 0, 0))
-
-        win.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
-        pygame.display.update()
-        pygame.time.delay(2000)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            for btn in btns:
-                if btn.click(pos) and game.connected():
-                    if player == 0:
-                        if not game.p1Went:
-                            n.send(btn.text)
-                    else:
-                        if not game.p2Went:
-                            n.send(btn.text)
-
-    redrawWindow(win, game, player)
 
 
 def menu_screen():
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from _thread import *
 
 from game import Game
 
-server = "192.168.1.74"
+server = "10.11.250.207"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,15 +14,15 @@ try:
 except socket.error as e:
     str(e)
 
-s.listen(0)
-print("Waiting for connection, Server Started")
+s.listen(2)
+print("Waiting for a connection, Server Started")
 
 connected = set()
 games = {}
 idCount = 0
 
 
-def thread_client(conn, player, gameId):
+def threaded_client(conn, p, gameId):
     global idCount
     conn.send(str.encode(str(p)))
 
@@ -40,13 +40,14 @@ def thread_client(conn, player, gameId):
                     if data == "reset":
                         game.resetWent()
                     elif data != "get":
-                        game.play(player, data)
-                    reply = game
+                        game.play(p, data)
+
                     conn.sendall(pickle.dumps(game))
             else:
                 break
         except:
             break
+
     print("Lost connection")
     try:
         del games[gameId]
@@ -71,4 +72,4 @@ while True:
         games[gameId].ready = True
         p = 1
 
-    start_new_thread(thread_client, (conn, p, gameId))
+    start_new_thread(threaded_client, (conn, p, gameId))
